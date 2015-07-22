e940e3332dbf804dadf0eb140d913e760515ac69
rookie <rookie125@163.com>
2015-07-22 15:56:46 +0800

    列表之间的滑动更改为手指抬起触发

--- src/calendar.js
+++ src/calendar.js
@@ -831,13 +831,6 @@
             function move(ev){
                 var oEv = ev.targetTouches ? ev.targetTouches[0] : (ev || event);
                 dir = oEv.pageX - disX;
-                if(silde)return false;
-
-                if(Math.abs(dir) >= needW){
-                    silde = true;
-
-                    callBack && callBack($this, dir);
-                }
 
                 oEv.preventDefault && oEv.preventDefault();
                 return false;
@@ -849,6 +842,14 @@
 
                 this.removeEventListener('touchmove', move, false);
                 this.removeEventListener('touchend', end, false);
+
+                if(silde)return false;
+
+                if(Math.abs(dir) >= needW){
+                    silde = true;
+
+                    callBack && callBack($this, dir);
+                }
             }
 
             this.onmousemove = move;
